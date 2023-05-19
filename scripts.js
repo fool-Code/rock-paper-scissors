39f3ba929b3b28797eb7ecd27117f0c77a7064e4
Carlos Batistela <cbhenry96@gmail.com>
2023-05-19 16:03:19 -0300

    Add new function and buttons

--- scripts.js
+++ scripts.js
@@ -1,69 +1,75 @@
 const shapes = ['rock', 'paper', 'scissor'];
 
-function computerPlay() {
-    let randomShape = shapes[Math.floor(Math.random() * 3)];
-    return randomShape;
-}
-const computerSelection = computerPlay();
+const gameRock = document.getElementById('gameRock');
+const gamePaper = document.getElementById('gamePaper');
+const gameScissor = document.getElementById('gameScissor');
+const resultElement = document.getElementById('result');
+const playerScoreElement = document.getElementById('playerScore');
+const computerScoreElement = document.getElementById('computerScore');
 
 let computerScore = 0;
 let playerScore = 0;
 
-function playRound(playerSelection, computerSelection) {
+gameRock.addEventListener('click', () => {
+    playRound('rock');
+});
+
+gamePaper.addEventListener('click', () => {
+    playRound('paper');
+});
+
+gameScissor.addEventListener('click', () => {
+    playRound('scissor');
+});
+
+function playRound(playerSelection) {
+    const computerSelection = computerPlay();
+    
     if (playerSelection === computerSelection) {
-        return 'Tie'
-    } else if (playerSelection == 'rock') {
-        if (computerSelection == 'paper'){
-            computerScore++;
-            return 'You Lose! Paper beats Rock'
-        } else if (computerSelection == 'scissor') {
-            playerScore++;
-            return 'You Won! Rock beats Scissor'
-        } else {
-            return 'It\'s a tie!'
-        }
-    } else if (playerSelection == 'paper') {
-        if (computerSelection == 'scissor'){
-            computerScore++;
-            return 'You Lose! Scissor beats Paper'
-        } else if (computerSelection == 'rock') {
-            playerScore++;
-            return 'You Won! Paper cover Rock'
-        } else {
-            return 'It\'s a tie!'
-        }
-    } else if (playerSelection == 'scissor') {
-        if (computerSelection == 'rock'){
-            computerScore++;
-            return 'You Lose! Rock beats Scissor'
-        } else if (computerSelection == 'paper') {
-            playerScore++;
-            return 'You Won! Scissor cut Paper'
-        } else {
-            return 'It\'s a tie!'
-        }
+        resultElement.textContent = 'It\'s a tie!';
+    } else if (
+        (playerSelection === 'rock' && computerSelection === 'scissor') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissor' && computerSelection === 'paper')
+    ) {
+        playerScore++;
+        resultElement.textContent = `You won! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        computerScore++;
+        resultElement.textContent = `You lost! ${computerSelection} beats ${playerSelection}`;
+    }
+
+    updateScores();
+    checkWinner();
+}
+
+function computerPlay() {
+    const randomIndex = Math.floor(Math.random() * shapes.length);
+    return shapes[randomIndex];
+}
+
+function updateScores() {
+    playerScoreElement.textContent = playerScore;
+    computerScoreElement.textContent = computerScore;
+}
+
+function checkWinner() {
+    if (playerScore === 5) {
+        resultElement.textContent = 'Congratulations! You won the game!';
+        disableButtons();
+    } else if (computerScore === 5) {
+        resultElement.textContent = 'Sorry, you lost the game!';
+        disableButtons();
     }
 }
 
-function game() {
-    computerScore = 0;
-    playerScore = 0;
-    for (let i = 0; i < 5; i ++) {
-        const playerSelection = prompt('Choose Rock Paper or Scissors!');
-        const computerSelection = computerPlay();
-        playRound(playerSelection, computerSelection)
-          
-    }
-        if(playerScore > computerScore){
-            console.log('You win the game')
-    }
-        else if(playerScore < computerScore){
-            console.log('You lose the game')
-    }
-        else if(playerScore === computerScore){
-            console.log('Draw')
-        }
+function disableButtons() {
+    gameRock.disabled = true;
+    gamePaper.disabled = true;
+    gameScissor.disabled = true;
 }
+
+
 
 // console.log(playRound('rock', computerSelection));
 // console.log(playRound('paper', computerSelection));
